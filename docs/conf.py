7081f7f74c06499abcfed78aae2bc63dc5d8e53b
financeupdates <nandininama57@gmail.com>
2026-07-16 17:45:35 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'Cheap Car Insurance'
+author = 'Cheap Car Insurance'
+release = '1.0'
+
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "Cheap Car Insurance 2026"
+
+# Sitemap
+html_baseurl = 
+sitemap_url_scheme = "{link}"
